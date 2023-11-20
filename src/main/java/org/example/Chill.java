eeb2b80d1c88fc9b3134e78364f6ba9998f7707f
Anton Stengaard <afstengaard@gmail.com>
2023-11-20 12:13:59 +0100

    Goddam crack math

--- src/main/java/org/example/Chill.java
+++ src/main/java/org/example/Chill.java
@@ -7,25 +7,36 @@ public class Chill {
 
     private FileIO io = new FileIO();
     private TextUI ui = new TextUI();
-    private User user1 = new User("Anton","1234",24);
-    public List<Movie> movieList = new ArrayList<>();
-    public List<Series> seriesList = new ArrayList<>();
+    private User currentUser;
+    public List<Movie> movieList = io.loadList();
+    public List<Series> seriesList = io.loadList();
 
+    //Saves the selected media to the users list of saved media.
     public void saveMedia(User u,Media m){
         u.savedMedia.add(m);
     }
 
-    public void watchMedia(Media m){
-        displayMessage("You're now watching: " + m.getTitle());
+    //"Plays" the selected media.
+    public void watchMedia(User u,Media m){
+        ui.displayMessage("You're now watching: " + m.getTitle());
+        u.watchedMedia.add(m);
     }
 
+    //Removes the selected media from the users list of saved media.
     public void removeMedia(User u, Media m){
         u.savedMedia.remove(m);
-        displayMessage(m.getTitle() + " has been removed from your saved list.");
+        ui.displayMessage(m.getTitle() + " has been removed from your saved list.");
     }
 
-    private void search(String query){
-
+    //Searches for a query and returns a list of media where the title contains the query
+    private List<Media> searchByName(String query,List<Media> list){
+        List<Media> searchedList = new ArrayList<>();
+        for(Media m:list){
+            if(m.getTitle().contains(query)){
+                searchedList.add(m);
+            }
+        }
+        return searchedList;
     }
 
 }
