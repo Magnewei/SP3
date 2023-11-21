744badb14d60aa13a78102f838890076c3763890
Magnewei <143735701+Magnewei@users.noreply.github.com>
2023-11-21 11:23:54 +0100

    -IO

--- src/main/java/org/example/IO.java
+++ src/main/java/org/example/IO.java
@@ -5,9 +5,11 @@ import java.util.List;
 public interface IO {
     public List<Media> loadSeries();
     public List<Media> loadMovies();
+    public List<User> loadUsers();
+    public List<String> loadUserMedia(User u);
     public List<Media> loadList();
     public void createUser(String username, String password, int age);
-    public void login(String username, String password);
+    public User login(String username, String password);
 }
 
 
